e4a236a398256eb9fad719a56449a02a650afe8f
Fernando Doglio <deleteman@gmail.com>
2022-03-01 14:37:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import Form  from 'react-bootstrap/Form'
+import Container from 'react-bootstrap/Container'
+import { Alert, Button } from 'react-bootstrap';
 
-function App() {
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const {Group, Label, Control} = {...Form}
+export default function App() {
+  const { register, handleSubmit, formState } = useForm();
+  const onSubmit = data => console.log(data);
+  
+  console.log(formState.errors)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+    <form onSubmit={handleSubmit(onSubmit)} className="simpleForm">
+      {/* alerting of errors */}
+      {(!formState.isValid && formState.isSubmitted) ?
+      <Alert variant="danger"   >
+        {Object.values(formState.errors).map( (e,idx) => {
+          console.log(e)
+          return (<p key={idx}>{e.message}</p>)
+        })}
+      </Alert>
+      :
+      <Alert variant="success"   >Please fill in the form</Alert>
+      }
+
+      <Group>
+        <Label>
+          First name
+        </Label>
+        <Control type="text" placeholder="First name" {...register("First name", {
+          required: {
+            value: true,
+            message: "You must specify your first name before moving forward"
+        }, 
+        pattern: {
+          value: /^[a-zA-Z]+$/,
+          message: "That's not a valid name where I come from..."
+        }
+        })} />
+      </Group>
+
+      <Group>
+        
+        <Label>Last Name</Label>
+        <Control type="text" placeholder="Last name" {...register("Last name", {
+          required: {
+            value:true,
+            message: "Please, add your last name"
+          },
+          pattern: {
+            value: /^[a-zA-Z]+$/,
+            message: "I don't think you understood exactly what to do here, did you?"
+          }, 
+          maxLength: {
+            value: 100,
+            message: "That's way too long to be a real last name, try again"
+         }
+        })} />
+      </Group>
+      <Group>
+        <Label>Email</Label>
+        <Control type="text" placeholder="Email" {...register("Email", {
+          required: {
+            value: true,
+            message: "You need to specify a valid email address"
+          }, 
+          pattern: {
+            value: /^\S+@\S+$/i,
+            message: "I think I said _valid_, didn't I?"
+         }
+        })} />
+      </Group>
+      <Group>
+        <Label>Mobile number</Label>
+        <Control  type="tel" placeholder="Mobile number" {...register("Mobile number", {
+          required: {
+            value: true,
+            message: "Please add your mobile phone number, I won't call you, promise!"
+          }, 
+          pattern: {
+            value: /^[0-9+-]+$/,
+            message: "This is not a valid mobile phone to me, try again!"
+          }, 
+          minLength: {
+            value: 6,
+            message: "This number is too short, not gotta fly, try again"
+          }, 
+          maxLength: {
+            value: 12,
+            message: "...And now it's too damn long, make sure the number is right, would you?"
+          } })} />
+      </Group>
+      <Group>
+        <Label>Comments</Label>
+        <Control as="textarea"  {...register("Your comments", {
+          required: {
+            value: true,
+            message: "I really wanna know what you think about this form, so leave a comment please!"
+          }, 
+          maxLength: {
+            value: 280,
+            message: "But don't over do it, 280 characters should be more than enough!"
+          }
+        })} />
+
+        <Button type="submit" >
+          Send
+        </Button>
+      </Group>
+    </form></Container>
   );
 }
-
-export default App;
